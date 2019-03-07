5e96d1e62630ed3717c80debc559f8bc4e473632
Yuchia Tseng <yuchia.tseng@irt-systemx.fr>
2019-03-07 20:22:19 +0100

    udpate

--- auto_start_kafka.sh
+++ auto_start_kafka.sh
@@ -4,5 +4,5 @@ xterm -hold -e "bin/zookeeper-server-start.sh  config/zookeeper.properties" &
 sleep 3
 xterm -hold -e "bin/kafka-server-start.sh config/server.properties" &
 sleep 3
-xterm -hold -e "bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic oai" &
-xterm -hold -e "bin/kafka-console-producer.sh --broker-list localhost:9092 --topic oai"
+xterm -hold -e "bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic oai-ts" &
+#xterm -hold -e "bin/kafka-console-producer.sh --broker-list localhost:9092 --topic oai"
