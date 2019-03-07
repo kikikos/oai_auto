6718bc69c78ac85df0b6e27702034c2bb9e6220e
Yuchia Tseng <yuchia.tseng@irt-systemx.fr>
2019-03-07 20:52:57 +0100

    update

--- auto_start_kafka.sh
+++ auto_start_kafka.sh
@@ -1,8 +1,8 @@
-cd /home/user/app/kafka_2.11-2.1.0; 
+dir=/home/yuchia/app/kafka_2.11-2.1.0; 
 
-xterm -hold -e "bin/zookeeper-server-start.sh  config/zookeeper.properties" &
+xterm -hold -e "$dir/bin/zookeeper-server-start.sh  $dir/config/zookeeper.properties" &
 sleep 3
-xterm -hold -e "bin/kafka-server-start.sh config/server.properties" &
+xterm -hold -e "$dir/bin/kafka-server-start.sh $dir/config/server.properties" &
 sleep 3
-xterm -hold -e "bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic oai-ts" &
-#xterm -hold -e "bin/kafka-console-producer.sh --broker-list localhost:9092 --topic oai"
+#xterm -hold -e "$dir/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic oai-ts" &
+#xterm -hold -e "$dir/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic oai"
